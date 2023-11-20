805aab72513a36b57801d3a934e6e8898a422e25
penaherrera <cpena@ioogo.com>
2023-11-20 15:08:21 -0600

    set controllers for user session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    include ActionController::Cookies
+
+
+    private
+
+    def require_login
+        render json: { error: 'Unauthorized' }, status: :unauthorized unless session[:user_id]
+    end
 end
