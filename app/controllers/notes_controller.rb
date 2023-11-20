51dac74a5332128b3d46885e15438c14ea6789c2
penaherrera <cpena@ioogo.com>
2023-11-20 16:19:55 -0600

    Adding NotesController actions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,11 @@
 class NotesController < ApplicationController
     before_action :require_login
+    
+    def index
+      notes = current_user.notes
+      render json: notes
+    end
+    
     def create
         @note = current_user.notes.new(note_params)
 
@@ -10,6 +16,26 @@ class NotesController < ApplicationController
         end
     end
 
+    def update
+      note = current_user.notes.find(params[:id])
+  
+      if note.update(note_params)
+        render json: note
+      else
+        render json: { errors: note.errors.full_messages }, status: :unprocessable_entity
+      end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Note not found' }, status: :not_found
+    end
+
+
+    def destroy
+      note = current_user.notes.find(params[:id])
+      note.destroy
+      render json: { message: 'Note deleted successfully' }
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Note not found' }, status: :not_found
+    end
 
     def note_params
         params.require(:note).permit(:title, :content)
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :reminders
-  resources :notes
+  
+  resources :notes do
+    get 'index', on: :collection
+  end
+
   resources :users, only: [:create] do
     post 'login', on: :collection
     delete 'logout', on: :collection
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
